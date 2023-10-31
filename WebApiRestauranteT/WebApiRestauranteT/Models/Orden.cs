62c861a2a70be661e157d870d81ee89639294400
Nou7en <tomh.tc89@gmail.com>
2023-10-31 11:19:41 -0500

    Primer commit

---
+++ WebApiRestauranteT/WebApiRestauranteT/Models/Orden.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiRestauranteT.Models
+{
+    public class Orden
+    {
+        [Key]
+        public int id { get; set; }
+        public int numOrden { get; set; }
+        public int idMesa { get; set; }
+        public Plato plato { get; set; }
+        public double precio { get; set; }
+
+
+    }
+}
---
+++ WebApiRestauranteT/WebApiRestauranteT/Models/Plato.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiRestauranteT.Models
+{
+    public class Plato
+    {
+        [Key]
+        public int idPlato { get; set; }
+        public string nombre { get; set; }
+        public double precio { get; set; }
+
+    }
+}
